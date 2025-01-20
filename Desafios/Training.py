67e2c1c867287c61e940b7c44a34118ff4876c40
MprogrammerZ <marcio.av.zanoni@gmail.com>
2025-01-20 11:08:24 -0500

    new exercises 19 and 20

--- Desafios/Training.py
+++ Desafios/Training.py
@@ -1,13 +1,45 @@
 #Practing the new functionalities learned
+import random
 
-#Desafio 19: Sorteie o nome de um aluno entre quatro opções.
-#Desafio 20: Embaralhe a ordem de uma lista de nomes.
+#Desafio 19: Sorteie o nome de um aluno entre quatro opções.(DONE)
+# a1 = input("Aluno 1 : ")
+# a2 = input("Aluno 2 : ")
+# a3 = input("Aluno 3 : ")
+# a4 = input("Aluno 4 : ")
+#
+# list = [a1, a2, a3, a4]
+# r = random.choice(list) # metodo choice da familia random, para escolher uma opcao dentre uma lista
+#
+# print(f"A ordem inicial eh de {list} e o aluno sorteado foi {r}")
+#Obs: Seleciona todas as linhas e aperta ctrl + / = comments everywhere
+
+
+#Desafio 20: Embaralhe a ordem de uma lista de nomes. (DONE)
+# a1 = input("Aluno 1 : ")
+# a2 = input("Aluno 2 : ")
+# a3 = input("Aluno 3 : ")
+# a4 = input("Aluno 4 : ")
+#
+# list = [a1, a2, a3, a4]
+# print(f"A lista inicial era {list}")
+#
+# random.shuffle(list)
+# print(f"A lista atualizada eh {list}")
+
+
 #Desafio 23: Leia um número de 0 a 9999 e mostre cada dígito separado (unidade, dezena, centena, milhar).
+
 #Desafio 24: Verifique se uma cidade começa com "Santo".
+
 #Desafio 27: Leia o nome completo e exiba o primeiro e último nome.
+
 #Desafio 28: Jogo da Adivinhação (adivinhe um número de 0 a 5).
+
 #Desafio 34: Calcule o novo salário de um funcionário com base em faixas de aumento.
+
 #Desafio 39: Leia a data de nascimento de uma pessoa e informe se ela precisa se alistar.
+
 #Desafio 45: Crie um jogo de Pedra, Papel e Tesoura.
+
 #Desafio 49: Mostre a tabuada de um número (usando for).
 
